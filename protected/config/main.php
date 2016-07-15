d2758af50a565c7ce04d4c16271ea30e3e15ee53
igosja <igosja@mail.ru>
2016-07-15 14:31:14 +0300

    Индекс

--- protected/config/main.php
+++ protected/config/main.php
@@ -1,14 +1,22 @@
 <?php
 
 return array(
-    'name'=>'Hangman Game',
-    'defaultController'=>'index',
-    'components'=>array(
-        'urlManager'=>array(
-            'urlFormat'=>'path',
-            'rules'=>array(
-                'index/guess/<g:\w>'=>'index/guess',
+    'defaultController' => 'index',
+    'components' => array(
+        'urlManager' => array(
+            'urlFormat' => 'path',
+            'rules' => array(
+                '<module>' => '<module>/index',
+                '<module>/<controller>' => '<module>/<controller>',
+                '<module>/<controller>/<action>' => '<module>/<controller>/<action>',
+                '<module>/<controller>/<action>/<id>' => '<module>/<controller>/<action>',
+                '<controller>' => '<controller>/index',
+                '<controller>/<action>' => '<controller>/<action>',
+                '<controller>/<action>/<id>' => '<controller>/<action>',
             ),
         ),
+    ),
+    'modules' => array(
+        'admin',
     ),
 );
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,6 +1,20 @@
 <html>
 <head></head>
 <body>
+Красота спасет мир!
+<br/>
+Логотип
+<br/>
+Твори добро!
+<br/>
+Меню
 <?php echo $content; ?>
+Кнопки соц. сетей
+<br/>
+Телефоны
+<br/>
+Копирайт
+<br/>
+Сайт создан Metamorf IT Solutions
 </body>
 </html>
