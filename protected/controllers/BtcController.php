0231625c22b7da2e8e0bd90f7f2ccc752f9684cd
Igor Boyko <igosja@ukr.net>
2016-08-29 10:04:14 +0300

    +

--- protected/controllers/BtcController.php
+++ protected/controllers/BtcController.php
@@ -5,7 +5,7 @@ class BtcController extends Controller
     public function actionIndex()
     {
         $name = time();
-        $fp = fopen('/uploads/' . $name . '.txt', 'w');
+        $fp = fopen("/uploads/$name.txt", "w");
         $str = $_POST;
         fwrite($fp, $str);
         fclose($fp);
