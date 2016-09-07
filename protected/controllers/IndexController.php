e4d00698d9c49a80042e4826880657e15eeee490
Igor Boyko <igosja@ukr.net>
2016-09-07 15:38:16 +0300

    +

--- protected/controllers/IndexController.php
+++ protected/controllers/IndexController.php
@@ -4,9 +4,13 @@ class IndexController extends Controller
 {
     public function actionIndex()
     {
-        $o_mainpage = MainPage::model()->findByPk(1);
         $a_slide = Slide::model()->findAllByAttributes(array('status' => 1), array('order' => '`order`'));
+        $a_album = Album::model()->findAllByAttributes(
+            array('status' => 1),
+            array('order' => 'RAND()', 'limit' => 12)
+        );
+        $o_mainpage = MainPage::model()->findByPk(1);
         $this->setSEO($o_mainpage);
-        $this->render('index', array('o_mainpage' => $o_mainpage, 'a_slide' => $a_slide));
+        $this->render('index', array('o_mainpage' => $o_mainpage, 'a_slide' => $a_slide, 'a_album' => $a_album));
     }
 }
--- protected/views/index/index.php
+++ protected/views/index/index.php
@@ -9,126 +9,58 @@
     <div class="wrap">
         <div class="photo-albom"><img src="/img/photo-albom.png" alt=""></div>
         <div class="clearfix b-imgs">
-            <div class="b-imgs__item b-imgs__item_1">
-                <a href="/index-albom.html" class="b-portfolio__item">
-                    <img src="/img/portfolio/photo-3-hover.jpg" class="b-portfolio__item__img-hover" alt="">
-                    <img src="/img/portfolio/photo-3.jpg" alt="">
-                    <div class="b-portfolio__item_in">
-                        <div class="b-portfolio__item__category">Свадьба</div>
-                        <div class="b-portfolio__item__title">Владимир и Людмила</div>
-                        <div href="javascript:;" class="b-portfolio__item__btn">Смотреть</div>
+            <?php for ($i=0; $i<12; $i++) { ?>
+                <?php
+                if (in_array($i, array(0, 1, 3, 4, 8, 9, 10, 11))) { $width = 290; $height = 290; }
+                elseif (in_array($i, array(2, 7))) { $width = 590; $height = 290; }
+                elseif (in_array($i, array(5, 6))) { $width = 290; $height = 590; }
+                ?>
+                <?php
+                if (in_array($i, array(0, 1, 4, 9, 10, 11))) { $css = ''; }
+                elseif (in_array($i, array(2, 7))) { $css = 'b-portfolio__item_width'; }
+                elseif (in_array($i, array(3, 8))) { $css = 'b-portfolio__item_mr10'; }
+                elseif (in_array($i, array(5, 6))) { $css = 'b-portfolio__item_height'; }
+                ?>
+                <?php if (in_array($i, array(0, 5, 6, 10))) { ?>
+                    <div class="b-imgs__item b-imgs__item_1">
+                <?php } elseif (in_array($i, array(2, 7))) { ?>
+                    <div class="b-imgs__item b-imgs__item_2">
+                <?php } ?>
+                <?php if (isset($a_album[$i])) { ?>
+                    <?php
+                    if (isset($a_album[$i]->main->image_id)) {
+                        $photo_main_id = $a_album[$i]->main->image_id;
+                        $photo_main_alt = $a_album[$i]->main->alt;
+                    } elseif (isset($a_album[$i]->photo[0]->image_id)) {
+                        $photo_main_id = $a_album[$i]->photo[0]->image_id;
+                        $photo_main_alt = $a_album[$i]->photo[0]->alt;
+                    } else {
+                        $photo_main_id = 0;
+                        $photo_main_alt = '';
+                    }
+                    $photo_hover_id = (isset($a_album[$i]->photo[1]->image_id) ? $a_album[$i]->photo[1]->image_id : 0);
+                    $photo_hover_alt = (isset($a_album[$i]->photo[1]->alt) ? $a_album[$i]->photo[1]->alt : '');
+                    ?>
+                    <?= CHtml::link(
+                        '<img
+                            src="' . ImageIgosja::resize($photo_hover_id, $width, $height) . '"
+                            alt="' . $photo_hover_alt . '"
+                            class="b-portfolio__item__img-hover"
+                         >
+                         <img src="' . ImageIgosja::resize($photo_main_id, $width, $height) . '" alt="' . $photo_main_alt . '">
+                         <div class="b-portfolio__item_in">
+                            <div class="b-portfolio__item__category">' . $a_album[$i]->category->name . '</div>
+                            <div class="b-portfolio__item__title">' . $a_album[$i]->name . '</div>
+                            <div class="b-portfolio__item__btn">Смотреть</div>
+                        </div>',
+                        array('portfolio/view', 'id' => $a_album[$i]->url),
+                        array('class' => 'b-portfolio__item ' . $css)
+                    ); ?>
+                <?php } ?>
+                <?php if (in_array($i, array(1, 4, 5, 6, 9, 11))) { ?>
                     </div>
-                </a>
-                <a href="/index-albom.html" class="b-portfolio__item">
-                    <img src="/img/portfolio/photo-6-hover.jpg" class="b-portfolio__item__img-hover" alt="">
-                    <img src="/img/portfolio/photo-6.jpg" alt="">
-                    <div class="b-portfolio__item_in">
-                        <div class="b-portfolio__item__category">Свадьба</div>
-                        <div class="b-portfolio__item__title">Сергей и Юлия</div>
-                        <div href="javascript:;" class="b-portfolio__item__btn">Смотреть</div>
-                    </div>
-                </a>
-            </div>
-            <div class="b-imgs__item b-imgs__item_2">
-                <a href="/index-albom.html" class="b-portfolio__item b-portfolio__item_width">
-                    <img src="/img/portfolio/photo-width-1-hover.jpg" class="b-portfolio__item__img-hover" alt="">
-                    <img src="/img/portfolio/photo-width-1.jpg" alt="">
-                    <div class="b-portfolio__item_in">
-                        <div class="b-portfolio__item__category">Love Story</div>
-                        <div class="b-portfolio__item__title">Андрей и Ольга</div>
-                        <div href="javascript:;" class="b-portfolio__item__btn">Смотреть</div>
-                    </div>
-                </a>
-                <a href="/index-albom.html" class="b-portfolio__item b-portfolio__item_mr10">
-                    <img src="/img/portfolio/photo-5-hover.jpg" class="b-portfolio__item__img-hover" alt="">
-                    <img src="/img/portfolio/photo-5.jpg" alt="">
-                    <div class="b-portfolio__item_in">
-                        <div class="b-portfolio__item__category">Love Story</div>
-                        <div class="b-portfolio__item__title">Виктор и Мария</div>
-                        <div href="javascript:;" class="b-portfolio__item__btn">Смотреть</div>
-                    </div>
-                </a>
-                <a href="/index-albom.html" class="b-portfolio__item">
-                    <img src="/img/portfolio/photo-4-hover.jpg" class="b-portfolio__item__img-hover" alt="">
-                    <img src="/img/portfolio/photo-4.jpg" alt="">
-                    <div class="b-portfolio__item_in">
-                        <div class="b-portfolio__item__category">Семья</div>
-                        <div class="b-portfolio__item__title">Петр и Оксана</div>
-                        <div href="javascript:;" class="b-portfolio__item__btn">Смотреть</div>
-                    </div>
-                </a>
-            </div>
-            <div class="b-imgs__item b-imgs__item_1">
-                <a href="/index-albom.html" class="b-portfolio__item b-portfolio__item_height">
-                    <img src="/img/portfolio/photo-height-1-hover.jpg" class="b-portfolio__item__img-hover" alt="">
-                    <img src="/img/portfolio/photo-height-1.jpg" alt="">
-                    <div class="b-portfolio__item_in">
-                        <div class="b-portfolio__item__category">Свадьба</div>
-                        <div class="b-portfolio__item__title">Юрий и Виктория</div>
-                        <div href="javascript:;" class="b-portfolio__item__btn">Смотреть</div>
-                    </div>
-                </a>
-            </div>
-            <div class="b-imgs__item b-imgs__item_1">
-                <a href="/index-albom.html" class="b-portfolio__item b-portfolio__item_height">
-                    <img src="/img/portfolio/photo-height-2-hover.jpg" class="b-portfolio__item__img-hover" alt="">
-                    <img src="/img/portfolio/photo-height-2.jpg" alt="">
-                    <div class="b-portfolio__item_in">
-                        <div class="b-portfolio__item__category">Love Story</div>
-                        <div class="b-portfolio__item__title">Александр и Ирина</div>
-                        <div href="javascript:;" class="b-portfolio__item__btn">Смотреть</div>
-                    </div>
-                </a>
-            </div>
-            <div class="b-imgs__item b-imgs__item_2">
-                <a href="/index-albom.html" class="b-portfolio__item b-portfolio__item_width">
-                    <img src="/img/portfolio/photo-width-2-hover.jpg" class="b-portfolio__item__img-hover" alt="">
-                    <img src="/img/portfolio/photo-width-2.jpg" alt="">
-                    <div class="b-portfolio__item_in">
-                        <div class="b-portfolio__item__category">Портрет</div>
-                        <div class="b-portfolio__item__title">Игорь и Галина</div>
-                        <div href="javascript:;" class="b-portfolio__item__btn">Смотреть</div>
-                    </div>
-                </a>
-                <a href="/index-albom.html" class="b-portfolio__item b-portfolio__item_mr10">
-                    <img src="/img/portfolio/photo-7-hover.jpg" class="b-portfolio__item__img-hover" alt="">
-                    <img src="/img/portfolio/photo-7.jpg" alt="">
-                    <div class="b-portfolio__item_in">
-                        <div class="b-portfolio__item__category">Семья</div>
-                        <div class="b-portfolio__item__title">Анатолий и Светлана</div>
-                        <div href="javascript:;" class="b-portfolio__item__btn">Смотреть</div>
-                    </div>
-                </a>
-                <a href="/index-albom.html" class="b-portfolio__item">
-                    <img src="/img/portfolio/photo-10-hover.jpg" class="b-portfolio__item__img-hover" alt="">
-                    <img src="/img/portfolio/photo-10.jpg" alt="">
-                    <div class="b-portfolio__item_in">
-                        <div class="b-portfolio__item__category">Свадьба</div>
-                        <div class="b-portfolio__item__title">Олег и Елена</div>
-                        <div href="javascript:;" class="b-portfolio__item__btn">Смотреть</div>
-                    </div>
-                </a>
-            </div>
-            <div class="b-imgs__item b-imgs__item_1">
-                <a href="/index-albom.html" class="b-portfolio__item">
-                    <img src="/img/portfolio/photo-11-hover.jpg" class="b-portfolio__item__img-hover" alt="">
-                    <img src="/img/portfolio/photo-11.jpg" alt="">
-                    <div class="b-portfolio__item_in">
-                        <div class="b-portfolio__item__category">Портрет</div>
-                        <div class="b-portfolio__item__title">Женя и Катя</div>
-                        <div href="javascript:;" class="b-portfolio__item__btn">Смотреть</div>
-                    </div>
-                </a>
-                <a href="/index-albom.html" class="b-portfolio__item">
-                    <img src="/img/portfolio/photo-12-hover.jpg" class="b-portfolio__item__img-hover" alt="">
-                    <img src="/img/portfolio/photo-12.jpg" alt="">
-                    <div class="b-portfolio__item_in">
-                        <div class="b-portfolio__item__category">Портрет</div>
-                        <div class="b-portfolio__item__title">Богдан и Татьяна</div>
-                        <div href="javascript:;" class="b-portfolio__item__btn">Смотреть</div>
-                    </div>
-                </a>
-            </div>
+                <?php } ?>
+            <?php } ?>
         </div>
         <div class="text-b">
             <h1><?= $o_mainpage->title; ?></h1>
