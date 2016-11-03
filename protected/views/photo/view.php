b8e7f0f92d7b7e2644c73d3da8c0916022a714bf
Boyko Igor <igosja@ukr.net>
2016-11-03 16:47:59 +0200

    share

--- protected/views/photo/view.php
+++ protected/views/photo/view.php
@@ -2,8 +2,8 @@
     <div class="wrap clearfix">
         <?= $this->renderPartial('/include/bread'); ?>
         <h1 class="m-title"><span><?= $o_photo->album->category->name; ?></span> <?= $o_photo->album->name; ?></h1>
-        <div class="photo-albom clearfix" id="inner">
-            <img src="<?= $o_photo->image->url; ?>" alt="<?= $o_photo->alt; ?>">
+        <div class="photo-albom clearfix" id="inner" style="max-width: 100%">
+            <img src="<?= ImageIgosja::resize($o_photo->image_id, 490, 490); ?>" alt="<?= $o_photo->alt; ?>">
         </div>
         <div class="text-b">
             <div class="text-b__soc text-b__soc_mt">
